ad472975d34ef5966b4260e61dd66f419d3668aa
Tumelo Mogono <tumelomogono@gmail.com>
2024-05-25 16:52:13 +0200

    completed the advanced exercises which I wrapped all the logic for each exercise inside a function which takes in the products array and returns an object containing all the results which I console log by calling the function inside it.

--- scripts.js
+++ scripts.js
@@ -17,69 +17,125 @@ const products = [
 
 //Exercise 1
 
-const namesAndProvinces =(names, provinces) => {
+// const namesAndProvinces =(names, provinces) => {
 
-  names.forEach(name =>
-    console.log(name)
-  );
+//   names.forEach(name =>
+//     console.log(name)
+//   );
 
-  provinces.forEach(province =>
-    console.log(province)
-  );
+//   provinces.forEach(province =>
+//     console.log(province)
+//   );
 
-  names.forEach((name, index) => {
-    console.log( `${name} (${provinces[index]})`)
-  });
-};
+//   names.forEach((name, index) => {
+//     console.log( `${name} (${provinces[index]})`)
+//   });
+// };
 
-namesAndProvinces(names, provinces);
+// namesAndProvinces(names, provinces);
 
-//Exercise 2
-const upperCaseProvinces = (provinces) => {
-  const newProvinceArray = provinces.map(province => province.toUpperCase());
-  console.log(newProvinceArray);
-}
+// //Exercise 2
+// const upperCaseProvinces = (provinces) => {
+//   const newProvinceArray = provinces.map(province => province.toUpperCase());
+//   console.log(newProvinceArray);
+// }
 
-upperCaseProvinces(provinces);
+// upperCaseProvinces(provinces);
 
-//Exercise 3
-const nameLenghts = (names) => {
-  const newNamesArray =  names.map(name => name.length);
-  console.log(newNamesArray);
-};
+// //Exercise 3
+// const nameLenghts = (names) => {
+//   const newNamesArray =  names.map(name => name.length);
+//   console.log(newNamesArray);
+// };
 
-nameLenghts(names);
+// nameLenghts(names);
 
-//Exercise 4
-const sortProvinces = (provinces) => {
-  const newSortedProvinces = provinces.sort();
+// //Exercise 4
+// const sortProvinces = (provinces) => {
+//   const newSortedProvinces = provinces.sort();
 
-  console.log(newSortedProvinces);
-};
+//   console.log(newSortedProvinces);
+// };
 
-sortProvinces(provinces);
-//Exercise 5
-const filteringCape = (provinces) => {
-  const newFilteredProvinces = provinces.filter(province => !province.includes("Cape"));
-  console.log(newFilteredProvinces.length)
-}
-filteringCape(provinces);
-//Exercise 6
-const findLetterInNames = (names) => {
-  const containsLetterS = names.map(name => name.split('').some(character => character.includes('s') || character.includes('S')));
-  console.log(containsLetterS)
-}
+// sortProvinces(provinces);
+// //Exercise 5
+// const filteringCape = (provinces) => {
+//   const newFilteredProvinces = provinces.filter(province => !province.includes("Cape"));
+//   console.log(newFilteredProvinces.length)
+// }
+// filteringCape(provinces);
+// //Exercise 6
+// const findLetterInNames = (names) => {
+//   const containsLetterS = names.map(name => name.split('').some(character => character.includes('s') || character.includes('S')));
+//   console.log(containsLetterS)
+// }
 
-findLetterInNames(names);
+// findLetterInNames(names);
 
-//Exercise 7
-const createObjectfromArrays = (names) => {
-  const newObjectFromArrays = names.reduce((accumulator, currentValue, index) => {
-    accumulator[currentValue] = provinces[index];
+// //Exercise 7
+// const createObjectfromArrays = (names) => {
+//   const newObjectFromArrays = names.reduce((accumulator, currentValue, index) => {
+//     accumulator[currentValue] = provinces[index];
+//     return accumulator
+//   },{});
+
+//   console.log(newObjectFromArrays);
+// };
+
+// createObjectfromArrays(names);
+
+
+///Advanced Exercises
+/**
+ * 
+ * @param {object} products 
+ * @returns {object}
+ */
+const advancedExercises = (products) => {
+  //exercise 1:
+  const logProducts = products.map(singleProduct => singleProduct.product).join(", ");
+
+  //exercise 2: 
+  const filteredNameByLength = products.filter(productName => productName.product.length <= 5);
+
+  //exercise 3:
+  const totalPrice = products.filter(individualProduct => individualProduct.price !== '' && individualProduct.price !== ' ').reduce((total, individualProduct) => total + Number(individualProduct.price), 0);
+
+  //exercise 4:
+  const concatenatedProductNames = products.reduce((accumulator, individualProduct) => accumulator + individualProduct.product);
+
+  //exercise 5:
+  const findExtremesInPrice = () => {
+    const pricedProducts = products.filter(individualProduct => individualProduct.price !== '' && individualProduct.price !== ' ').map(individualProduct => ({...individualProduct, price: Number(individualProduct.price)}));
+    const highestPrice = pricedProducts.reduce((max, individualProduct) => individualProduct.price > max.price ? individualProduct : max, pricedProducts[0]);
+    const lowestPrice = pricedProducts.reduce((min, individualProduct) => individualProduct.price < min.price ? individualProduct : min, pricedProducts[0]);
+    return `Highest Price is: ${highestPrice.product}. Lowest Price is: ${lowestPrice.product}.`
+  }
+
+  const highestAndLowestPrice = findExtremesInPrice();
+
+  //exercise 6:
+  const objectTransformation = products.reduce((accumulator, product) => {
+    Object.entries(product).forEach(([key, value]) => {
+      if (key === 'product') {
+        accumulator.push({ name: value });
+      } else if (key === 'price') {
+        accumulator[accumulator.length -1].cost = value;
+      }
+    });
     return accumulator
-  },{});
+  }, []);
 
-  console.log(newObjectFromArrays);
-};
+  return {
+    logProducts,
+    filteredNameByLength,
+    totalPrice,
+    concatenatedProductNames,
+    highestAndLowestPrice,
+    objectTransformation
+  }
 
-createObjectfromArrays(names);
+}
+
+console.log(advancedExercises(products))
+// console.log(products.filter(productName => productName.product.length <= 5))
